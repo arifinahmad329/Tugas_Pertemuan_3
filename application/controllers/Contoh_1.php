63bd269124135d4c03f3c52e66114474519b2d9f
arifinahmad329 <arifinahmad329@gmail.com>
2024-04-15 18:37:10 +0700

    first commit

---
+++ application/controllers/Contoh_1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh_1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h2>Perkenalkan</h2>";
+        echo"Nama saya Achmad Arifin
+            Saya tinggal di daerah Jakarta
+            olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
+} 
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Achmad</li>
+        <li>Nama Belakang : Arifin</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kp Buaran Jatinegara jakarta timur</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Renang</li>
+    </ul>
+</section> 
